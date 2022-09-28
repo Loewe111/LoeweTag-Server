edf54f4a9b60a8b3a01f7ee38dc642b094551d79
Loewe111 <m.loewe111@gmail.com>
2022-09-28 20:47:13 +0200

    commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello World!")
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+          }
+    })
+
+    win.loadFile('ui/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    console.info("Closing App")
+    app.quit()  //Quit app when window is closed
+})
