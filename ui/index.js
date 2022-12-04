c05e02969cbbcdc9d15baee3da835704cd126224
Loewe111 <m.loewe111@gmail.com>
2022-12-04 10:40:28 +0100

    random values in leaderboard

--- ui/index.js
+++ ui/index.js
@@ -68,3 +68,4 @@ async function connectSerial(){
 main()
 setup()
 setInterval(main, 1000)
+
--- ui/leaderboard.js
+++ ui/leaderboard.js
@@ -1,10 +1,63 @@
-teams = {
-  1234: 1,
-  5678: 1,
-  1111: 2,
-  1122: 2,
-  3445: 4,
-  1233: 5,
-  1555: 4,
-  1239: 4
+//create an object containing the team ids and their names
+teamNames = {
+  "team-red": "Team Red",
+  "team-blue": "Team Blue",
+  "team-green": "Team Green",
+  "team-yellow": "Team Yellow",
+  "team-purple": "Team Purple",
+  "team-cyan": "Team Cyan",
+  "team-orange": "Team Orange",
+};
+
+function generateTeamLeaderboard(id, name, players) {
+  if(!players) return -1;
+  //Overall score
+  parent = document.getElementById(id);
+  headerDiv = document.createElement("div");
+  headerDiv.classList = "d-flex justify-content-between mx-1 mb-1";
+  header = document.createElement("h2");
+  header.innerHTML = name;
+
+  var sum = 0;
+  players.forEach((player) => {
+    sum += player.score;
+  });
+  scoreSum = document.createElement("h2");
+  scoreSum.innerHTML = sum;
+
+  headerDiv.appendChild(header);
+  headerDiv.appendChild(scoreSum);
+  parent.innerHTML = "";
+  parent.appendChild(headerDiv);
+
+  //Scores for each player
+
+  //sort players by score
+  players.sort((a, b) => {
+    return b.score - a.score;
+  });
+
+  players.forEach((player) => {
+    playerDiv = document.createElement("div");
+    playerDiv.classList = "d-flex justify-content-between mx-1";
+    playerName = document.createElement("h4");
+    playerName.innerHTML = player.name;
+    playerScore = document.createElement("h4");
+    playerScore.innerHTML = player.score;
+    playerDiv.appendChild(playerName);
+    playerDiv.appendChild(playerScore);
+    parent.appendChild(playerDiv);
+  });
+
+  //show the leaderboard
+  parent.classList.remove("hidden");
 }
+
+// test all teams with random scores
+Object.entries(teamNames).forEach(([id, name]) => {
+  generateTeamLeaderboard(id, name, [
+    { name: "Player 1", score: Math.floor(Math.random() * 100) },
+    { name: "Player 2", score: Math.floor(Math.random() * 100) },
+    { name: "Player 3", score: Math.floor(Math.random() * 100) },
+  ]);
+});
